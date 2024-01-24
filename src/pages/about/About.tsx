34e9278ca9223677f5794c07952e54db2d40fa94
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-01-24 10:00:13 +0100

    add title to about page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,5 +1,13 @@
 import './About.css'
 
 export default function About() {
-	return <div>About</div>
+	return (
+		<div className='about-page'>
+			<h1>
+				Canal Street Market is a carefully curated retail market, food hall &
+				community space open year-round at 265 Canal Street. Support Small
+				Business this weekend!
+			</h1>
+		</div>
+	)
 }
