514d1e4f0e488b706ee9ed2bb0a4bf8a2f5454bc
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-01-25 10:44:37 +0100

    add the fadeIn animation with the useintersectionobserver hook

---
+++ src/hooks/UseIntersectionObserver.ts
@@ -0,0 +1,35 @@
+import { MutableRefObject, useEffect } from 'react'
+
+export function UseIntersectionObserver(
+	refs: MutableRefObject<HTMLElement | null>[]
+) {
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						entry.target.classList.add('in-view')
+						observer.unobserve(entry.target)
+					}
+				})
+			},
+			{
+				threshold: 0.4,
+			}
+		)
+
+		refs.forEach((ref) => {
+			if (ref.current) {
+				observer.observe(ref.current)
+			}
+		})
+
+		return () => {
+			refs.forEach((ref) => {
+				if (ref.current) {
+					observer.unobserve(ref.current)
+				}
+			})
+		}
+	}, [refs])
+}
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,16 +1,22 @@
+import { useRef } from 'react'
 import './About.css'
+import { UseIntersectionObserver } from '../../hooks/UseIntersectionObserver'
 
 export default function About() {
+	const imageRef = useRef(null)
+
+	UseIntersectionObserver([imageRef])
+
 	return (
 		<div className='about-page'>
-			<section className='title'>
+			<section className='title-section'>
 				<h1>
 					Canal Street Market is a carefully curated retail market, food hall &
 					community space open year-round at 265 Canal Street. Support Small
 					Business this weekend!
 				</h1>
 			</section>
-			<section className='image'>
+			<section className='image-section' ref={imageRef}>
 				<img
 					src='https://images.prismic.io/canalstreetmarket/1d99cf0e3a0144c1229f92ee5b3c63b0eaae893c_home-hero.jpg?auto=compress,format'
 					alt='image1'
