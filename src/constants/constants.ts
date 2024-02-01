a7a7c96c6bbc77557f80038ccc8d2d3f4bdfa526
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-02-01 10:57:45 +0100

    add food plates to the constant

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -32,3 +32,113 @@ export const linkCards = [
 		text: 'Follow us on Instagram',
 	},
 ]
+
+export const foodPlates = [
+	{
+		type: 'Khao Man Gai',
+		name: 'Betong',
+		principalPhoto:
+			'https://images.prismic.io/canalstreetmarket/056a1e39-af19-4901-9738-36ac5abbb0f9_Betong.png?auto=compress,format',
+		secondaryPhotos: [
+			'https://images.prismic.io/canalstreetmarket/f48ec466-919e-4eeb-bf0a-d861ed92d4bb_Betong.jpg?auto=compress,format',
+			'https://images.prismic.io/canalstreetmarket/8e87d276-7b06-4891-837a-81c3305058ac_Betong+2.jpg?auto=compress,format',
+			'https://images.prismic.io/canalstreetmarket/b7288248-c622-4a32-a1bb-a85fce8c75f5_Betong+3.jpg?auto=compress,format',
+			'https://images.prismic.io/canalstreetmarket/e5b39a88-8fd9-4dae-bc89-38fe88b46ff1_Betong+4.jpg?auto=compress,format',
+		],
+		description:
+			"Khao Man Gai is a treasured Thai variation of Hainanese Chinese rice, a dish that is extremely popular through Southeast Asia. Betong's Khao Man Gai is a reflection of Chef Noon's childhood growing up in Betong, Thailand.",
+	},
+	{
+		type: 'Coffee, Pastries & Smoothies',
+		name: 'Chinatown Deli',
+		principalPhoto:
+			'https://images.prismic.io/canalstreetmarket/c9a661b5b3a3f08fba455f06839c5bc2153550d4_food-vendor-new-york.png?auto=compress,format',
+		secondaryPhotos: [
+			'https://images.prismic.io/canalstreetmarket/a23fdb39afc303c89ee0312e6ca3b5705ad3e2e4_new-york-food-market-49.jpg?auto=compress,format',
+			'https://images.prismic.io/canalstreetmarket/30dad87bf5025d9160f63bebc7b1d61f832eeb32_new-york-food-market-52.jpg?auto=compress,format',
+		],
+		description: 'Your favorite neighborhood coffee shop',
+	},
+	{
+		type: 'Artisanal Chocolate',
+		name: 'Daniel Corpuz Chocolatier',
+		principalPhoto:
+			'https://images.prismic.io/canalstreetmarket/e8af6535-7849-4464-83b2-0dc7fe451b4b_corpuz+chocolatier.png?auto=compress,format',
+		secondaryPhotos: [
+			'https://images.prismic.io/canalstreetmarket/21304cd6-316d-49bd-8016-e72021f91fdc_Daniel+Corpuz.jpg?auto=compress,format',
+			'https://images.prismic.io/canalstreetmarket/5fc02165-9e77-4bb0-a9b0-e4f04eea6029_dcc_csm.jpg?auto=compress,format',
+			'https://images.prismic.io/canalstreetmarket/06b25a96-a427-4b16-9bc1-42aadec7c93e_dcc1.jpeg?auto=compress,format',
+			'https://images.prismic.io/canalstreetmarket/7b1b8cdf-f8b7-4de0-a1b8-216e070bc4da_dcc2.jpeg?auto=compress,format',
+		],
+		description:
+			'Daniel Joseph Corpuz is a pastry chef and chocolatier based in New York City. As a graduate of the Culinary Institute of America, Daniel achieved his Associates Degree in Baking and Pastry and his Bachelor’s Degree in Food Business Administration in 2019. Daniel has worked in several fine dining restaurants in New York City, of which include The Modern at MoMA, Manhatta, and One White Street. As a result of the pandemic, Daniel has shifted to work primarily with chocolate, creating bonbons, confections, and showpieces. His extensive work with chocolate, during a turbulent time in the food industry yielded the opportunity to be on Netflix’s new show, ‘School of Chocolate’ with world renowned Pastry Chef Amaury Guichon. Utilizing his Filipino-American background, he ultimately decided to launch Daniel Corpuz Chocolatier. This chocolate brand centers on his upbringing while amplifying his industry experience by utilizing Filipino and Asian ingredients and flavors into his chocolates.',
+	},
+	{
+		type: 'Napoli Style Pizza',
+		name: 'Enzo Bruni La Pizza Gourmet',
+		principalPhoto: '',
+		secondaryPhotos: ['', ''],
+		description: 'Enzo Bruni is a pizzaiolo from Naples, Italy.',
+	},
+	{
+		type: 'Chinese Classics',
+		name: "Joe's Rice and Noodles",
+		principalPhoto: '',
+		secondaryPhotos: ['', ''],
+		description: '',
+	},
+	{
+		type: 'Cantonese Steamed Rice Rolls',
+		name: '',
+		principalPhoto:
+			'https://images.prismic.io/canalstreetmarket/02074214fef758434124320a62917c03c8490581_new-york-food-market-rice-roll.png?auto=compress,format',
+		secondaryPhotos: [
+			'https://images.prismic.io/canalstreetmarket/dacc0a87-c923-454d-a82e-f9a20d5d8b4d_SELECTS+HIGH+Res+_65.jpg?auto=compress,format',
+			'https://images.prismic.io/canalstreetmarket/06163891-f669-4ccf-a826-dea122d9f512_SELECTS+HIGH+Res+_30.jpg?auto=compress,format',
+		],
+		description:
+			'Joe’s Steam Rice Roll started with Chef and Owner Joe Rong’s longing for a steamed rice roll—the way he had it growing up in China: fresh rice batter poured onto a tray, along with fresh meat and vegetables, and all cooked within a steamer. Serendipitously, Joe’s quest for the ideal steamed rice roll led him back home to Taishan, China, where he learned from masters of the craft as a humble student. What we do differently is how we make our rice batter: fresh rice grinded daily by a stone mill, and combined with our in-house secret mix to produce a one-of-a-kind soft and chewy texture. Our mission is to continuously improve and innovate to share our best version of an authentic taste of our culture with the World.',
+	},
+	{
+		type: 'Delicious Filipino Classics',
+		name: 'Kabisera',
+		principalPhoto: '',
+		secondaryPhotos: ['', ''],
+		description:
+			'Since 2017, Kabisera has undergone a constant stream of reiterations and the ever-changing menu and frequent pop-up collaborations with other Filipino chefs and artists reflect that. Kabisera’s current mood is in offering a turo-turo style menu on top of their usual paninis, rice bowls, and salads, and utilizing their space to showcase specialty food products and artwork from local Filipino artists and entrepreneurs. Their pastries and kakanins, however, are what first put them on the map—like the biko, a sweet cake that uses glutinous rice and is topped with latik, a syrup made of coconut cream that will have you reassess your relationship with caramel.',
+	},
+	{
+		type: 'Bignsoo Sundaes & Bubble Tea',
+		name: 'Lazy Sundaes',
+		principalPhoto:
+			'https://images.prismic.io/canalstreetmarket/012a4823-3a84-499c-8bde-2da6d3b91565_LazySundaes.png?auto=compress,format',
+		secondaryPhotos: [
+			'https://images.prismic.io/canalstreetmarket/d7a5f452-2e4b-4049-926f-7f31fa3e3626_LazySundaes_BingsooGroup1_MinuHan.jpg?auto=compress,format',
+		],
+		description:
+			'Lazy Sundaes is a refuge from the bustle of your day; an afternoon pick-me-up, the sweet spot in your evening. We believe in celebrating the small victories; life’s hard, and you’ve earned it.',
+	},
+	{
+		type: 'Sushi HandRolls & Chirasi',
+		name: 'Mastunori',
+		principalPhoto:
+			'https://images.prismic.io/canalstreetmarket/70e149b3-3d82-46ce-a09f-59e2b1c1b36f_mastunori.png?auto=compress,format',
+		secondaryPhotos: [],
+		description:
+			"Matsunori is the kind of restaurant that attracts New Yorkers who like to be in the know. A hidden gem, you'll find fresh seafood, expertly prepared by a team of chefs that bring expertise from some of the finest sushi joints in the city. Take your friends and pull up a chair at the bar, where you'll be able to chat with the staff and watch dishes come to life. Specialty dishes include Chef's Choice Chirashi and Hand Rolls.",
+	},
+	{
+		type: 'Filipino Inspired Taqueria',
+		name: 'Mucho Sarap',
+		principalPhoto:
+			'https://images.prismic.io/canalstreetmarket/f14a4da6-69de-4f1c-a609-ac87f894c6c4_muchosarap.png?auto=compress,format',
+		secondaryPhotos: [
+			'https://images.prismic.io/canalstreetmarket/ff7d4c8a-c38a-4d06-9366-348b6148aff1_mucho1.png?auto=compress,format',
+			'https://images.prismic.io/canalstreetmarket/e29f3939-8ae1-46ed-afee-ed2522cec617_mucho2.png?auto=compress,format',
+			'https://images.prismic.io/canalstreetmarket/08542b51-4825-46e3-810f-5db777542f09_mucho3.png?auto=compress,format',
+			'https://images.prismic.io/canalstreetmarket/b508b914-f366-4470-ad82-d029d23fae84_mucho4.png?auto=compress,format',
+		],
+		description:
+			"Here at So Sarap's first brick-and-mortar, chef/owner VJ Navarro melds street food of the Philippines along with Mexican flavors for offerings like Taco de Longaniza (sweet Filipino sausage) and Taco de Nilaga (beef short ribs, cheek, and tongue).",
+	},
+]
