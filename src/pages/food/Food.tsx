0bfd24230c396a9bf4536dfba5f82b1f8d153e0b
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-02-01 10:18:55 +0100

    add the title to the food page

--- src/pages/food/Food.tsx
+++ src/pages/food/Food.tsx
@@ -5,11 +5,28 @@ export default function Food() {
 		<div className='food-page'>
 			<section className='hero-section'>
 				<div className='hero-title'>
-					<p></p>
-					<h1></h1>
+					<p>
+						Food <span>{'\u21E2'}</span>
+					</p>
+					<h1>The Food Hall</h1>
 				</div>
-				<div className='hero-image'></div>
+				<div className='hero-image'>
+					<div className='image-text'>
+						<p>
+							Food Hall Hours:
+							<br />
+							Mon – Sun: 11:00AM - 8:00PM
+						</p>
+						<p>餐饮</p>
+					</div>
+					<img
+						src='https://images.prismic.io/canalstreetmarket/13a988aff3e9c672350fe8330eb7386c3a85eeb9_home_page_three.jpg?auto=compress,format'
+						alt='Food img 1'
+					/>
+				</div>
 			</section>
+
+			<section className='plates-section'></section>
 		</div>
 	)
 }
