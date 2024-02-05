a42409066b0250c6b9cbf6f501c9d9f1dd618394
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-02-05 09:43:43 +0100

    add page title to retail page

--- src/pages/retail/Retail.tsx
+++ src/pages/retail/Retail.tsx
@@ -1,5 +1,18 @@
+import PageTitle from '../../components/pageTitle/PageTitle'
 import './retail.css'
 
 export default function Retail() {
-	return <div>Retail</div>
+	return (
+		<div className='retail-page'>
+			<section className='hero-section'>
+				<PageTitle
+					name='Retail'
+					title='Retail Market'
+					time='Thrus - Sun: 11:00AM - 7:00PM'
+					letter='購物'
+					imgSrc='https://images.prismic.io/canalstreetmarket/20c7997073b456fda0e6de872d23b6183892bdde_home_page_two.jpg?auto=compress,format'
+				/>
+			</section>
+		</div>
+	)
 }
