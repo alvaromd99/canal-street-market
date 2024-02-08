a896b65d55a1d5b1edc429607b894f8a52214248
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-02-08 10:40:25 +0100

    start with the food page css

--- src/pages/product/FoodPage.tsx
+++ src/pages/product/FoodPage.tsx
@@ -1,13 +1,26 @@
 import { foodPlates } from '../../constants/constants'
 import { findProductsByName } from '../../utils/findProductByName'
+import './allProducts.css'
 
 export default function FoodPage({ name }: { name: string }) {
 	const plate = findProductsByName(foodPlates, name)
 	console.log(plate)
 
 	return (
-		<div>
-			<h1>Hello world</h1>
+		<div className='product-info-page'>
+			{plate && (
+				<section className='hero-section'>
+					<div className='hero-left'>
+						<button>all food</button>
+						<h2>{plate.name}</h2>
+						<p>{plate.description}</p>
+					</div>
+					<div className='hero-right'>
+						<p>Food Hall Hours: Mon - Sun: 10AM- 8PM</p>
+						<img src={plate.principalPhoto} alt='plate photo' />
+					</div>
+				</section>
+			)}
 		</div>
 	)
 }
